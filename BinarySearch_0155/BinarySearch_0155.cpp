8197cf644f1689f49855c67d1c23a36167b6e47a
Rangga1011 <ranggaramadhana1011@gmail.com>
2024-06-13 10:47:44 +0700

    Menambhakan input Constructor node class

--- BinarySearch_0155/BinarySearch_0155.cpp
+++ BinarySearch_0155/BinarySearch_0155.cpp
@@ -9,5 +9,14 @@ public:
 	Node* leftchild;
 	Node* rightchild;
 
+	// Constructor for the node class
+
+	Node(string i, Node* l, Node* r)
+	{
+		info = i;
+		leftchild = l;
+		rightchild = r;
+}
+
 };
 
