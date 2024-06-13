db992e92dcc9466ce632462bd0cfc3e779510d5e
Rangga1011 <ranggaramadhana1011@gmail.com>
2024-06-13 11:18:14 +0700

    Menambahakn input void search

--- BinarySearch_0155/BinarySearch_0155.cpp
+++ BinarySearch_0155/BinarySearch_0155.cpp
@@ -56,5 +56,24 @@ public:
 			parent->rightchild = newNode;  // make the right child of the parent point to the new node
 		}
 	}
+
+	void search(string element, Node*& parent, Node*& currentNode)
+	{
+		// this function searches the currentNode of the specified Node as well as the current Node is parent
+		currentNode = ROOT;
+		parent = nullptr;
+		while ((currentNode != nullptr) && (currentNode->info != element))
+		{
+			parent = currentNode;
+			if (element < currentNode->info)
+				currentNode = currentNode->leftchild;
+			else
+				currentNode = currentNode->rightchild;
+		}
+	}
+
+	
 };
+
+
 
